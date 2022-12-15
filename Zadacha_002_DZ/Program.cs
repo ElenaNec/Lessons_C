4f4ddce0f25a9f92c4deda4a78fa7ba785994917
Elena_Nechaeva <you@example.com>
2022-12-15 23:04:36 +0300

    Задача 2 максимум из двух чисел

---
+++ Zadacha_002_DZ/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу, которая на вход принимает два числа и выдает максимальное
+Console.Clear();
+Console.Write("Введите первое число ");
+int number1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number1 > number2)
+{
+   Console.WriteLine($"Число {number1} максимальное"); 
+}
+else
+{
+    Console.WriteLine($"Число {number2} максимальное");
+};
