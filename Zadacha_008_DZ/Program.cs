ef90b5461bcc8163b7d1953ed427019eaba8210e
Elena_Nechaeva <you@example.com>
2022-12-16 18:09:37 +0300

    Задача 8 Вывод всех четных чисел находящихся от 1 до заданного числа

---
+++ Zadacha_008_DZ/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine()!);
+
+int index = 2;
+
+if (number < index)
+{
+    Console.WriteLine($"Нет четных чисел");
+}
+while (index <= number)
+{
+    Console.Write($" {index} ");
+    index = index + 2;
+};
