704668d04bcc5a87d8be6a851b874420fcb6738d
Elena_Nechaeva <you@example.com>
2022-12-16 17:43:40 +0300

    Задача 6 узнать четное или нечетное заданное число

---
+++ Zadacha_006_DZ/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Clear();
+
+Console.Write("Введите число ");
+int number1 = int.Parse(Console.ReadLine()!);
+
+if ( number1 % 2 == 0)
+{
+    Console.WriteLine($"Число {number1} четное");
+}    
+else
+{
+    Console.WriteLine($"Число {number1} нечетное"); 
+};
