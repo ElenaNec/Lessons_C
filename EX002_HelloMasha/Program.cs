80f51054ac0628cf1de3035a5f11b4d4b236a79c
Elena_Nechaeva <you@example.com>
2022-12-19 14:11:28 +0300

    Исправлена программа приветствия Маши

---
+++ EX002_HelloMasha/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа приветствия Маши (не важно какими буквами написано имя)
+
+Console.Write("Введите ваше имя ");
+string username = Console.ReadLine()!;
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это же Маша!");
+}
+else
+{
+    Console.Write($"Привет, {username}");
+}
