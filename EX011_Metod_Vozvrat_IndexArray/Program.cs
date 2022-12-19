3c29c0c69b6b58910e638b74406b4535c22e17ee
Elena_Nechaeva <you@example.com>
2022-12-19 17:01:15 +0300

    Добавлен метод возвращающий индекс массива

---
+++ EX011_Metod_Vozvrat_IndexArray/Program.cs
@@ -0,0 +1,20 @@
+﻿// Метод возвращающий позицию (индекс) массива
+
+int Indexof(int[] collection, int find )
+{
+    // определяем кол-во эл-тов
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+
+    while ( index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;  // Выход как только эл-т массива стал равен искомому числу
+        }
+        index++;
+    }
+    return position;
+}
