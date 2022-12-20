acb16ac5c48e81e274452d3156a6343fe86c3763
Elena_Nechaeva <you@example.com>
2022-12-20 19:57:47 +0300

    Добавлена задача про выходной день

---
+++ DZ_K_SEM_2/Zadacha_015_DZ/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает 
+на вход цифру, обозначающую день недели, и проверяет,
+является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Clear();
+// функция получения случайного числа
+int GetRangeNumber(int start, int end)
+{
+    int number = new Random().Next(1,end+1);
+    return number;
+}
+
+int rangeNumber = GetRangeNumber(1,7);
+
+if (rangeNumber < 6)
+{
+    Console.WriteLine($"Цифра {rangeNumber} показывает, что сегодня рабочий день");
+}
+else
+{
+    Console.WriteLine($"Цифра {rangeNumber} показывает, что сегодня выходной день");
+}
