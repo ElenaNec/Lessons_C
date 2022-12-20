5dec0ed411c59ce67d2cf2a7f9efbc87855bc7b3
Elena_Nechaeva <you@example.com>
2022-12-20 17:35:47 +0300

    В решение задачи добавлен метод

--- Zadacha_012/Program.cs
+++ Zadacha_012/Program.cs
@@ -1,19 +1,30 @@
-﻿//*12 Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+﻿//*12 Напишите программу, которая будет принимать на вход два числа и выводить,
+// является ли второе число кратным первому. 
+//Если число 2 не кратно числу 1, то программа выводит остаток от деления.
 
 //34, 5 -> не кратно, остаток 4 
 //16, 4 -> кратно 
 
 Console.Clear();
 
+Console.WriteLine("Введите первое число ");
 int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-int x = number1 % number2;
-if ( x == 0)
+// метод нахождения остатка от деления
+int KratnieChisla(int number1, int number2)
+{   int ostatok = number1 % number2;
+    return ostatok;
+}
+
+int ost = KratnieChisla(number1,number2);
+if ( ost == 0)
 {
-    Console.WriteLine("Кратно");
+     Console.WriteLine($"Числа {number1} и {number2} кратны");
 }
 else
 {
-    Console.WriteLine($"Не кратно, остаток от деления {x}");
+    Console.WriteLine($"Числа {number1} и {number2} не кратны, остаток от деления {ost}");
 }
