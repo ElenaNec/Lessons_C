1835934ab1efa011f2c80a61e37e603f3f48453e
Elena_Nechaeva <you@example.com>
2022-12-24 16:04:48 +0300

    Добавлена задача нахождения расстояния между двумя точками в пространстве

--- Zadacha_021/Program.cs
+++ Zadacha_021/Program.cs
@@ -4,3 +4,22 @@
 A (3,6); B (2,1) -> 5,09 
 A (7,-5); B (1,-1) -> 7,21
 */
+
+Console.Clear();
+
+//  функция выводит сообщение и считывает введенное с клавиатуры число (величину)
+int GetUserValue(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int x1 = GetUserValue("Веедите x1: ");
+int y1 = GetUserValue("Веедите y1: ");
+int x2 = GetUserValue("Веедите x2: ");
+int y2 = GetUserValue("Веедите y2: ");
+
+double sqrt = Math.Sqrt((x2-x1)*(y2-y1)); // вычисляет квадратный корень из выражения
+double sqrt1 = Math.Round(sqrt,2); // отделяет 2 символа после запятой
+Console.WriteLine($"Расстояние = {sqrt1}");
