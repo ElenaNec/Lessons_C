d1d3516aac6ee2f22feba22fec272e2bb1dfa367
Elena_Nechaeva <you@example.com>
2022-12-25 13:07:20 +0300

    Добавлена задача 18 указывает диапазон в котором может находится точка при заданной четверти

---
+++ Zadacha_018/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Напишите программу, кот по заданному номеру чеверти 
+показывает диапазон возможных координат точки в этой четверти
+*/
+
+
+int GetUserValue(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+string GetRageFromQuatre(int quarter)
+{
+    string result = $"Диапазон значений в {quarter} четверти ";
+    switch (quarter)
+    {
+        case 1 :
+        {
+            result += "x>0 и y>0"; 
+            break;
+        }
+        case 2 :
+        {
+            result += "x<0 и y>0"; 
+            break;
+        }
+        case 3 :
+        {
+            result += "x<0 и y<0"; 
+            break;
+        }
+        case 4 :
+        {
+            result += "x>0 и y<0"; 
+            break;
+        }
+        default:
+        {
+            result = $"Такой четверти не существует";
+            break;
+        }
+    }
+    return result;
+}
+
+int quarter = GetUserValue("Введите номер четверти");
+string rangeDescription = GetRageFromQuatre(quarter);
+Console.WriteLine(rangeDescription);
