e5e4a24718a9d42ea5b67d5c15eb32399b552883
Elena_Nechaeva <you@example.com>
2023-01-17 20:28:45 +0300

    Добавлены задачи (заполнение случайными числами двумер массива, нахождение факториала, числа Фибоначи) из лекции 4

---
+++ EX_Factorial/Program.cs
@@ -0,0 +1,8 @@
+﻿// Вычисление факториала
+
+double Factorial(int n) // для больших чисел используют double
+{
+    if (n==1) return 1;
+    else return n* Factorial(n-1);
+}
+Console.WriteLine(Factorial(5));
---
+++ EX_Print_Dvum_Arr/Program.cs
@@ -0,0 +1,34 @@
+﻿// Заполнение случайными числами и печать двумерного массива 
+
+Console.Clear();
+
+// Метод печати двумерной матрицы
+void printArray(int[,] matr)
+{
+    for (int i=0; i<matr.GetLength(0); i++) // matr.GetLength(0) Возвращает кол-во строк в массиве
+    {
+    for (int j=0; j<matr.GetLength(1); j++) //matr.GetLength(1) Возвращает кол-во столбцов в массиве
+    {
+        Console.Write($"{matr[i,j]} ");
+    }
+    Console.WriteLine();    
+    }
+}
+ 
+ //Метод заполнения массива случайными числами
+void FillArray(int[,] matr)
+{
+    for (int i=0; i<matr.GetLength(0); i++)
+    {
+        for (int j=0; j<matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10); // [1,10)
+        }
+    }
+}
+
+int[,] matrix = new int[3,4];
+printArray(matrix);
+Console.WriteLine();
+FillArray(matrix);
+printArray(matrix);
